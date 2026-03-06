c09991c0c9e91de39bf13b03fe4f4aa2c61a7220
taefi <taefi.soroosh@gmail.com>
2026-03-06 11:44:02 +0200

    fix log4j provider conflict

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,13 +1,15 @@
 package com.example;
 
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
-import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Theme("default")
+@StyleSheet(Lumo.STYLESHEET)
+@StyleSheet(Lumo.UTILITY_STYLESHEET)
 @Push
 public class Application implements AppShellConfigurator {
 
