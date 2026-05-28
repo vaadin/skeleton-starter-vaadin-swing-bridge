e67ffd63c28b63c877464ee3494b1f10fa8c6c14
Wolfgang Fischlein <wolfgang@vaadin.com>
2026-05-28 16:33:15 +0200

    May28th-session

--- src/main/java/com/example/swingbridge/ui/WarehouseView.java
+++ src/main/java/com/example/swingbridge/ui/WarehouseView.java
@@ -1,15 +1,21 @@
 package com.example.swingbridge.ui;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
-import com.vaadin.modernization.swing.bridge.component.SwingBridge;
+import com.vaadin.swingbridge.SwingBridge;
 
 @Route(value = "")
 public class WarehouseView extends VerticalLayout {
-    private static final String MAIN_CLASS = "com.mycompany.swingapp.warehouse.WarehouseInventoryApp2";
+    private static final String MAIN_CLASS = "com.mycompany.swingapp.warehouse.WarehouseInventoryApp3";
 
     public WarehouseView() {
         add(new SwingBridge(MAIN_CLASS));
+        add(new Button(() -> {
+            SwingBridge.interop().of(com.mycompany.swingapp.warehouse.WarehouseInventoryApp3Bridge.class)
+                    .requestAsync(pWarehouseInventoryApp3Bridge -> pWarehouseInventoryApp3Bridge.selectForm("Offers"));
+            return null;
+        }));
     }
 
 }
